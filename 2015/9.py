348e8e7ebdd8ef3fb808e00448f3495dea581c0c
Bram Van de Walle <bram.vandewalle@otainsight.com>
2021-12-02 21:58:41 +0100

    AoC 2015

--- 2015/9.py
+++ 2015/9.py
@@ -1,37 +1,58 @@
 import re
+import itertools
 
 
-class Node:
-    def __init__(self, id):
-        self.id = id
-        self.neighbours = []
-
-    def add_neighbour(self, node):
-        self.neighbours.append(node)
-
-
-class Graph:
-    def __init__(self):
-        self.nodes = dict()
-
-    def get_or_create_node(self, id):
-        if id in self.nodes:
-            return self.nodes[id]
-
-        self.nodes[id] = Node(id)
-        return self.nodes[id]
-
-
-def setup(config):
-    graph = Graph()
-
-    for line in config:
-        m = re.search(r'^(\w+) to (\w+) = (\d+)$', line)
-        f, t, d = m.groups()  # from, to, distance
-        n1 = graph.get_or_create_node(f)
-        n2 = graph.get_or_create_node(t)
-
+def get_cost(graph, permutation):
+    cost = 0
+    for idx in range(1, len(permutation)):
+        frm = permutation[idx-1]
+        to = permutation[idx]
+        cost += graph[frm][to]
+    return cost
 
 
 with open('files/9.txt', 'r') as f:
-    config = f.readlines()
+    lines = f.readlines()
+
+graph = dict()
+cities = set()
+
+for line in lines:
+    m = re.search(r'^(\w+) to (\w+) = (\d+)$', line)
+    frm, to, cost = m.groups()
+    cost = int(cost)
+
+    # just blindly add, its a set...
+    cities.add(frm)
+    cities.add(to)
+
+    if frm not in graph:
+        graph[frm] = {to: cost}
+    else:
+        graph[frm][to] = cost
+
+    if to not in graph:
+        graph[to] = {frm: cost}
+    else:
+        graph[to][frm] = cost
+
+permutations = list(itertools.permutations(cities))
+
+current_min = get_cost(graph, permutations[0])
+current_min_perm = permutations[0]
+
+current_max = current_min
+current_max_perm = current_min_perm
+
+for perm in permutations[1:]:
+    if (cost := get_cost(graph, perm)) < current_min:
+        current_min = cost
+        current_min_perm = perm
+    elif cost > current_max:
+        current_max = cost
+        current_max_perm = perm
+
+print(current_min_perm)
+print(current_min, '\n')
+print(current_max_perm)
+print(current_max)
